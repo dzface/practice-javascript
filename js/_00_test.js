421dbf4fc6cf8d45df7226fc5b6e7b826676ade1
D code <note37@naver.com>
2024-04-25 00:53:20 +0900

    2024-04-25

--- js/_00_test.js
+++ js/_00_test.js
@@ -1,3 +1,20 @@
 document.write("HTML에 자바스크립트 사용");
 console.log("브라우저 디버깅 창에서 결과 확인");
 alert("경고메세지");
+
+let value1 = 'someValue1';
+let value2 = 'someValue2';
+let value3 = 'newValue';
+
+let object = {
+  property1: value1,
+  property2: value2,
+  method1: function(){},
+  method2: function(){},
+};
+
+object['property1'] = value3;
+
+console.log(object['property1']);
+
+
