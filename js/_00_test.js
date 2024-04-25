41697cbc3cb68aec8c3eda6a86b3f1af1cd16f6a
D code <note37@naver.com>
2024-04-25 15:49:11 +0900

    2024-04-25

--- js/_00_test.js
+++ js/_00_test.js
@@ -1,20 +1,25 @@
-document.write("HTML에 자바스크립트 사용");
-console.log("브라우저 디버깅 창에서 결과 확인");
-alert("경고메세지");
-
-let value1 = 'someValue1';
-let value2 = 'someValue2';
-let value3 = 'newValue';
-
-let object = {
-  property1: value1,
-  property2: value2,
-  method1: function(){},
-  method2: function(){},
-};
-
-object['property1'] = value3;
-
-console.log(object['property1']);
+// 반복문 방법: 기본, for-in, for-of
 
 
+// 방법1. 기본
+let brands = ["애플", "구글", "페이스북", "아마존", "삼성전자"];
+for(let i = 0; i < brands.length; i++){
+  console.log(brands[i]);
+}
+
+// 방법2. for-in: key-value가 있는 객체에 사용
+let person = {fname:"Jone", lname:"Doe", age:25};
+for (const key in person) {
+  console.log(person[key]);
+}
+
+// 방법3. for-of
+let actors = ["송강호", "이미나", "하정우", "이지은", "손석구"];
+for (const actor of actors) {
+  console.log(actor); 
+}
+
+let lang = 'Javascript';
+for (const e of lang) {
+  console.log(e);
+}
---
+++ js/jquery.js
@@ -0,0 +1,15 @@
+// const el =document.querySelector("#myEl").innerHTML;
+// console.log(el);
+
+// const els= document.querySelectorAll(".myEls");
+// for(let e of els){
+//   console.log(e.innerHTML);
+// }
+
+// JQuery 스타일로 변경하기
+const $el = $("#myElement");
+console.log($el.text());
+console.log($el.css("background-color"));
+
+const $els = $(".myEls");
+console.log($els);
